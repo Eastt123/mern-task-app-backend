dd10bc3c06c132067f6acd71828e1c73089ee779
Eastt123 <58124751+Eastt123@users.noreply.github.com>
2023-09-18 03:20:23 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
     origin:["http://localhost:3000",
-        "https://mern-task-app-1pbi.onrender.com"
+        "https://mern-task-app-meip.onrender.com/"
 ]
 }))
 
